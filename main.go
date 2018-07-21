cf6f3be5aa243f6641e5f9c8c68e95d204177700
BumBimz Ks <l3uml3imz@gmail.com>
2018-07-21 14:31:54 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/bumbimz/gotools"
+	"github.com/bumbimz/gotools/stringhelper"
+)
+
+func init() {
+	fmt.Println("init")
+}
+
+func main() {
+	fmt.Println("Hello World")
+	fmt.Println(gotools.Add(10, 2))
+	fmt.Println(stringhelper.Upper("Dog"))
+}
